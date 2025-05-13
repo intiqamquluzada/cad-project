92bb5dbc9235a3028eacde9eca655bc6adc2264d
Intigam Guluzade <quluzadintiqam@gmail.com>
2025-05-13 14:51:35 +0400

    admin credentials

--- cad/management/commands/create_superuser.py
+++ cad/management/commands/create_superuser.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(email="admin@gmail.com").exists():
             User.objects.create_superuser(email="admin@gmail.com", password="1234")
             self.stdout.write(self.style.SUCCESS("Superuser created"))
         else:
